13b51dbc727db7fd53daec91b1ed7e1c1c1d5342
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-22 19:25:25 +0300

    DOM Assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+document.body.style.backgroundColor='silver'; 
+
+
+document.getElementById('title').style.color='green';
+
+document.getElementById("title-tag1").style.textTransform = "uppercase";
+document.getElementById("title-tag2").style.textTransform = "uppercase";
+
+let fruit = document.createElement('li');
+fruit.innerHTML = 'Kiwi'
+document.getElementById('fruList').appendChild(fruit);
+
+
+let vegetable = document.createElement('li');
+vegetable.innerHTML = 'Lettuce';
+document.getElementById('vegList').appendChild(vegetable);
+
+document.body.style.fontFamily='Arial';
+document.body.style.fontSize ='20px';
+
+
+
